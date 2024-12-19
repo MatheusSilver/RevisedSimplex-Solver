9dfe4f8300e569ac0b56868407607279acfe1f23
Matheus <matheus.feitosa16@outlook.com>
2024-12-19 19:04:39 -0300

    Fixes e finalização Solver

--- src/Solver.py
+++ src/Solver.py
@@ -12,6 +12,8 @@ class RevisedSimplex:
         self.objective = data["objective_function"]
         self.restrictions = data["restrictions_vector"]
         self.restriction_symbols = data["symbols"]
+        self.slack_variables = []
+        self.artificial_variables = []
 
     def solve(self, show_steps: bool = False):
 
@@ -25,17 +27,42 @@ class RevisedSimplex:
             from_phase_one = True
         self.__solve_phase_two(from_phase_one, show_steps)
 
+    def __get_initial_artificial_basis(self) -> list[str]:
+        number_of_variables = len(self.variables)
+        current_artificial_index = 0
+        initial_basis = []
+        for i in (range(len(self.slack_variables))):
+            if self.variable_values[number_of_variables + i] != 0:
+                initial_basis.append(self.slack_variables[i])
+            else:
+                initial_basis.append(self.artificial_variables[current_artificial_index])
+                current_artificial_index += 1
+        while current_artificial_index < len(self.artificial_variables):
+            initial_basis.append(self.artificial_variables[current_artificial_index])
+            current_artificial_index += 1
+
+        return initial_basis
+
+    def __get_initial_artificial_non_basis(self, initial_basis: list[str]) -> list[str]:
+        initial_non_basis = []
+        possible_variables = self.variables + self.slack_variables+self.artificial_variables
+        for i in (range(len(possible_variables))):
+            if possible_variables[i] not in initial_basis:
+                initial_non_basis.append(possible_variables[i])
+        return initial_non_basis
+
+
 
     def __solve_phase_one(self, show_steps: bool = False) -> int:
         if len(self.artificial_variables) < 1:
             return -1
-        # Lucro auxiliar: minimizar soma das variáveis artificiais
+        
         artificial_costs = np.zeros(len(self.variables) + len(self.slack_variables) + len(self.artificial_variables))
         artificial_costs[-len(self.artificial_variables):] = 1  # Custos das artificiais
 
         # Configuração inicial
-        self.basis = self.artificial_variables.copy()
-        self.non_basis = self.variables.copy() + self.slack_variables.copy()
+        self.basis = self.__get_initial_artificial_basis()
+        self.non_basis = self.__get_initial_artificial_non_basis(self.basis)
         profit = artificial_costs
 
         # Matriz básica inicial
@@ -67,69 +94,60 @@ class RevisedSimplex:
 
         result = self.__solver_loop(b, basic_indexes, non_basic_indexes, profit, self.restrictions, False, show_steps)
 
+        #Todo: Implementar um indicador melhor disso.
+        if result == 0 and self.basis == self.slack_variables:
+            print("problema não factível")
+            return
+
         if result == 0:
             print("Solução ótima encontrada!")
             print("Variáveis básicas:", self.basis)
-            print("Valores:", self.__get_solution())
+            print("Valores:", self.get_solution())
 
-    def __get_solution(self) -> dict:
+    def get_solution(self) -> dict:
         variables_list = self.variables + self.slack_variables + self.artificial_variables
         return {variables_list[i]: self.variable_values[i] for i in range(len(variables_list))}
 
-    # Se 0, então a solução ótima foi encontrada.
     def __solver_loop(self, basic_matrix: np.ndarray[np.float64], basic_indexes: list[int],
                       non_basic_indexes: list[int], profit_vector: np.ndarray[np.float64],
                       restrictions_vector: np.ndarray[np.float64], is_phase_one: bool, show_steps: bool = False) -> int:
 
-        # Garante que o vetor seja uma coluna
         restrictions_vector = restrictions_vector.reshape(-1, 1)
         variables_list = self.variables + self.slack_variables + self.artificial_variables
 
         while True:
             inv_b = np.linalg.inv(basic_matrix)
 
-            # Soluções básicas
             x_b = inv_b @ restrictions_vector
 
-            # Lucro básico
             p_t = profit_vector[basic_indexes] @ inv_b
 
-            # Custos reduzidos
-
             c_r = profit_vector[non_basic_indexes] - p_t @ self.constraint_matrix[:, non_basic_indexes]
-            # Identifica variável de entrada
             in_index = self.__get_negative_pivot(c_r, show_steps)
             if in_index == -1:
-                print("Solução ótima encontrada")
                 return 0  # Código de sucesso
 
-            # Calcula direções
             c_n = self.constraint_matrix[:, in_index].reshape(-1, 1)
             y = inv_b @ c_n
             if np.all(y <= 0):
                 print("Solução ilimitada detectada")
                 return -1  # Solução ilimitada
 
-            # Calcula razões e identifica variável de saída
             ratios = np.where(y > 0, x_b / y, np.inf)
             out_index = self.__get_positive_pivot(ratios, show_steps)
 
-            # Atualiza índices básicos e não básicos
             out_index_basic = basic_indexes[out_index]
             in_index_non_basic = non_basic_indexes[in_index]
 
             basic_indexes[out_index], non_basic_indexes[in_index] = self.__update_variable_values(basic_indexes, variables_list, x_b, y,
                                                                                         out_index, in_index_non_basic, out_index_basic)
 
-            # Atualiza matriz básica
             basic_matrix[:, out_index] = self.constraint_matrix[:, in_index_non_basic]
 
             if show_steps:
                 print(f"Iteração concluída: variável {in_index_non_basic} entrou, {out_index_basic} saiu")
 
-
-
-            if is_phase_one and not basic_indexes in self.artificial_variables:
+            if is_phase_one and not any(index in self.artificial_variables for index in basic_indexes):
                 return 0
 
     def __update_variable_values(self, basic_indexes, variables_list: list[str], x_b, y, out_index,
@@ -161,32 +179,34 @@ class RevisedSimplex:
         variables_list = self.variables + self.non_basis + self.artificial_variables
         return [variables_list.index(var) for var in self.non_basis]
 
-    def __get_basic_profits(self, basic_indexes: list[int], costs: np.ndarray[np.float64]) -> np.ndarray[np.float64]:
+    @staticmethod
+    def __get_basic_profits(basic_indexes: list[int], costs: np.ndarray[np.float64]) -> np.ndarray[np.float64]:
         return costs[basic_indexes]
 
-    def __get_negative_pivot(self, relative_costs: np.ndarray[np.float64], show_steps: bool = False) -> int:
+    @staticmethod
+    def __get_negative_pivot(relative_costs: np.ndarray[np.float64], show_steps: bool = False) -> int:
         negative_indexes = np.where(relative_costs < 0)[0]
         if negative_indexes.size == 0:
             return -1
 
         min_index = negative_indexes[np.argmin(relative_costs[negative_indexes])]
 
-        return min_index
+        return int(min_index)
 
-    def __get_positive_pivot(self, ratios: np.ndarray[np.float64], show_steps: bool = False) -> int:
+    @staticmethod
+    def __get_positive_pivot(ratios: np.ndarray[np.float64], show_steps: bool = False) -> int:
         positive_indexes = np.where(ratios > 0)[0]
         if positive_indexes.size == 0:
             return -1
         min_index = positive_indexes[np.argmin(ratios[positive_indexes])]
 
-        return min_index
+        return int(min_index)
 
     def __standardize_problem(self, show_steps: bool = False) -> None:
         self.artificial_variables = []
         self.slack_variables = []
 
         num_restrictions = len(self.restriction_symbols)
-        num_variables = len(self.variables)
         artificial_lines = []
         slack_lines = []
         for i in range(num_restrictions):
@@ -209,8 +229,6 @@ class RevisedSimplex:
                 self.artificial_variables.append(artificial_variable)
                 artificial_lines.append(i)
 
-        # Garantindo que a matriz será montada na ordem de primeiro as slacks depois as artificiais
-        # O objetivo é facilitar os pops e a impressão em latex depois.
         for i in slack_lines:
             if i in artificial_lines:
                 self.__add_variable_to_matrix(i, -1)
@@ -228,7 +246,7 @@ class RevisedSimplex:
 
     def __add_variable_value_to_vector(self, line_to_add: int, value: int) -> None:
         restriction_value = self.restrictions[line_to_add]
-        if(value==-1):
+        if value==-1:
             self.variable_values.append(0)
         else:
             self.variable_values.append(restriction_value)
@@ -236,4 +254,5 @@ class RevisedSimplex:
     def __remove_artificial_variables(self):
         for i in range(len(self.artificial_variables)):
             self.variable_values.pop()
+            self.non_basis.remove(self.artificial_variables[i])
             self.artificial_variables.pop()
